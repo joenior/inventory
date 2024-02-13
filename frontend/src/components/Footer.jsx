2325f0af9891962f426c329c4c137a5744479fc0
Steven <stevenjoeniorz@gmail.com>
2024-02-13 22:46:52 +0700

    tugas sistem terdistribusi

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="main-footer">
+            <div className="footer-left">
+                Copyright &copy; 2024 </div>
+            <div className="footer-right">v1.0</div>
+        </footer>
+    );
+}
---
+++ frontend/src/config/appConfig.js
@@ -0,0 +1,8 @@
+const appConfig = {
+    debounceTimeout: 750,
+    expirationTime: 60 * 60 * 1000, // set 1 hours
+    baseURL: "http://192.168.163.163:8000",
+    baseurlAPI: "http://192.168.163.163:8000/api",
+};
+
+export default appConfig;
